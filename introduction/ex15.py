1841fe2f720c27703c106cf9bc75cb246df08967
Ben Hong <bencodezen@gmail.com>
2019-02-28 05:51:07 -0500

    feature: Setup ex15 exercise

---
+++ introduction/ex15.py
@@ -0,0 +1,13 @@
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+
+print(f"Here's your file {filename}:")
+print(txt.read())
+
+print("Type the filename again:")
+file_again = input("> ")
+
+print(txt_again.read())
