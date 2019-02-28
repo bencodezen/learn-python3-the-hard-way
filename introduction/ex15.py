1b023640d6ee562d6a9cc03fb1520e0244df59d5
Ben Hong <bencodezen@gmail.com>
2019-02-28 05:55:06 -0500

    doc: Explain code

--- introduction/ex15.py
+++ introduction/ex15.py
@@ -1,13 +1,18 @@
 from sys import argv
 
+# Get the filename
 script, filename = argv
 
+# Opens the file and sets it to a variable
 txt = open(filename)
 
 print(f"Here's your file {filename}:")
 print(txt.read())
 
+# This allows users to call another file
 print("Type the filename again:")
 file_again = input("> ")
+
+txt_again = open(file_again)
 
 print(txt_again.read())
