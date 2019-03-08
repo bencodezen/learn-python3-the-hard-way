10bcafbbb3d0b215a386d2422f5aaa34880a7659
Ben Hong <bencodezen@gmail.com>
2019-03-08 07:46:38 -0500

    feature: Scaffold basic template

--- introduction/ex50/app.py
+++ introduction/ex50/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  greeting = "World"
-  return f'Hello, {greeting}!'
+  greeting = "Test"
+  return render_template("index.html", greeting=greeting)
 
 if __name__ == "__main__":
   app.run()
